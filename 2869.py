34cf74aeb037b8702104f574022c635a792700b4
taeyoon-cloud <xodbs448@hanyang.ac.kr>
2021-04-18 22:22:03 +0900

    baekjoon_2869

--- 2869.py
+++ 2869.py
@@ -1,14 +1,15 @@
 #2869번: 달팽이는 올라가고 싶다
 
-import sys
-import math
 
-up, down, height = map(int, sys.stdin.readline().split())
+up, down, height = map(int, input().split())
 
 day = (height - up) / (up - down) + 1
 
-print(math.ceil(day))
+if day == int(day): # day가 정수일 경우 그 값을 출력
+    print(int(day))
+else:
+    print(int(day) + 1) # day가 정수가 아닐경우 day보다 큰 첫번째 정수를 출력
 
-# math.ceil(day) -> 올림 함수
-# day 값 이상의 첫번째 정수가 답
+# 아니면 math.ceil(day) -> 올림 함수를 사용해서 출력해도 됨
+
 
